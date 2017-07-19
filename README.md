ba6dd1717b3f902d77bf98a8ea8fee15767fda05
Joe Fieger <joe.fieger@gmail.com>
2017-07-18 19:38:52 -0700

    test is functional and readme updated
    also included is a minor update to gitignore for pycharm ide

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 # repo-tweet
 A tool which sends notifications via twitter for PRs posted in the repository
 
+
+## Objective
 HTTP Integration with GitHub and Twitter
 Write a program that will poll a GitHub repo, look for new pull requests, and tweet a summary of each PR to Twitter. Some practical considerations:
 * A dummy github repo should be created and API access be enabled
 * A dummy twitter account should be created and API access should be enabled
 * Getting the PRs via webhook might be impractical (especially considering that we should be able to easily run and test your program). Suggestion: the program can start up, gather the latest PR's, post them to Twitter, then exit. Subsequent runs of the program should pick up PR's that have come in since the last time. (How will you persist the state between runs?) .... Alternately, the program can run forever and periodically poll the GitHub repo for changes.
 
-Constraints
+## Constraints
 * Make plain HTTP requests against GitHub and Twitter, i.e., do not use a 3rd party Github or Twitter SDK for Python. The python requests library is recommended (http://docs.python-requests.org/en/master/
 
+
+## Installation / Setup (tested on MacOS)
+pip install virtualenv
+virtualenv ENV
+cd ENV
+source bin/activate
+pip install -r requirements.txt
+
+**Note** if a virtual environment isn't available or not desired, only the last step is needed.
+
+
+## Running the test
+python main.py
--- main.py
+++ main.py
@@ -24,61 +24,81 @@
 """
 
 import json
+import os
 import requests
-import urllib
+
+import twitter
+
+PRIVATE_DATA_FILE = 'private.json'
 
 # github
-github_api_url = 'https://api.github.com/repos/jfieger/repo-tweet'
-github_auth_token = '74c374337f008a53ccd60a253ff4c899ca07de46'  # read-only api access only
+# github_api_url = 'https://api.github.com/repos/jfieger/repo-tweet'
+# github_auth_token = '74c374337f008a53ccd60a253ff4c899ca07de46'  # read-only api access only
 
 # twitter
-twitter_account = 'tweettest4234'
-twitter_api_url = 'https://api.twitter.com/1.1/search/tweets.json?q=%s' % twitter_account
-twitter_bearer_credentials = 'MHJrVllLbzNVcDk3Z2pVT3FKTThCdHR4WDp3YmY1dWpPeGFKbHVsRHVwNlVtUlptV2w3d1NZeVdacWpxcEwwZDJhdUZpQjVhemttNQ=='
+# twitter_account = 'tweettest4234'
+# twitter_api_url = 'https://api.twitter.com/1.1/search/tweets.json?q=%s' % twitter_account
+# twitter_bearer_credentials = 'MHJrVllLbzNVcDk3Z2pVT3FKTThCdHR4WDp3YmY1dWpPeGFKbHVsRHVwNlVtUlptV2w3d1NZeVdacWpxcEwwZDJhdUZpQjVhemttNQ=='
 
 
-def github_get_prs():
-    """
-        Return a list of strings of the following format:
-            <user.login> has created a PR in <base.repo.full_name>: <body> : <html_url>
-
-        If there are no PRs, returns an empty list
-    """
-    headers = {'Authorization': 'token %s' % github_auth_token}
-    r = requests.get('%s/pulls?state=all' % github_api_url, headers=headers)
-
-    prs = []
-    # print(r.url)
-    if r.status_code == 200:
-        data = json.loads(r.content)
 
 
-        for i in range(0, len(data)):
-            # print(data[i]['html_url'])
-            login = data[i]['user']['login']
-            full_name = data[i]['base']['repo']['full_name']
-            body = data[i]['body']
-            url = data[i]['html_url']
-            pr_string = '%s has created a PR in %s: %s - %s' % (login, full_name, body, url)
-            # print(pr_string)
-            prs.append(pr_string)
-            # <user.login> has created a PR in <base.repo.full_name>: <body> : <html_url>
-    else:
-        print('ERROR: failed to retrieve github prs')
-
-    return prs
-
-
-class Twitter():
+class GitTwitty():
 
     def __init__(self):
-        self.account = twitter_account
-        self.bearer_credentials = twitter_bearer_credentials
-        self.set_bearer_token()
+        self.load_private_data()
+        self.twitter_set_bearer_token()
 
-    def set_bearer_token(self):
+        # self.account = twitter_account
+        # self.bearer_credentials = twitter_bearer_credentials
+        # self.account = twitter_account
+        # self.bearer_credentials = twitter_bearer_credentials
+
+    def load_private_data(self):
+        with open(PRIVATE_DATA_FILE) as priv_file:
+            self.private_data = json.load(priv_file)
+
+    def github_get_prs(self):
+        """
+            Return a list of strings of the following format:
+                <user.login> has created a PR in <base.repo.full_name>: <body> : <html_url>
+
+            If there are no PRs, returns an empty list
+        """
+        token = self.private_data['github']['api_token']
+
+        url = 'https://api.github.com/repos/%s' % self.private_data['github']['repo_name']
+
+        headers = {'Authorization': 'token %s' % token}
+        r = requests.get('%s/pulls?state=all' % url, headers=headers)
+
+        prs = []
+        # print(r.url)
+        # print(r.content)
+        if r.status_code == 200:
+            data = json.loads(r.content)
+
+            if len(data) == 0:
+                print("WARNING: no prs found")
+            else:
+                for i in range(0, len(data)):
+                    # print(data[i]['html_url'])
+                    login = data[i]['user']['login']
+                    full_name = data[i]['base']['repo']['full_name']
+                    body = data[i]['body']
+                    url = data[i]['html_url']
+                    pr_string = '%s has created a pull request in %s: %s - %s' % (login, full_name, body, url)
+                    # print(pr_string)
+                    prs.append(pr_string)
+                    # <user.login> created a pull request in <base.repo.full_name>: <body> : <html_url>
+        else:
+            print('ERROR: failed to retrieve github prs')
+
+        return prs
+
+    def twitter_set_bearer_token(self):
         url = 'https://api.twitter.com/oauth2/token'
-        headers = {'Authorization': 'Basic %s' % self.bearer_credentials,
+        headers = {'Authorization': 'Basic %s' % self.private_data['twitter']['bearer_credentials'],
                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
         data = 'grant_type=client_credentials'
         r = requests.post(url, headers=headers, data=data)
@@ -95,16 +115,19 @@ class Twitter():
             print('ERROR: failed to retreive bearer token')
             self.bearer_token = None
 
-    def get_timeline(self):
+    def twitter_get_timeline(self):
         """
             :return: return json blob, else None
         """
         if self.bearer_token is None:
             return None
 
-        url = 'https://api.twitter.com/1.1/statuses/user_timeline.json?count=100&screen_name=%s' % self.account
+        url = 'https://api.twitter.com/1.1/statuses/user_timeline.json?count=100&screen_name=' + \
+              self.private_data['twitter']['screen_name']
+
         headers = {'Authorization': 'Bearer %s' % self.bearer_token,
                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
+
         r = requests.get(url, headers=headers)
         # print(r.url)
         # print(r.status_code)
@@ -119,42 +142,47 @@ class Twitter():
 
         return tweets
 
-    def tweet(self, msg):
+    def twitter_status_update(self, msg):
         """
-            POST https://api.twitter.com/1.1/statuses/update.json?status=Maybe%20he%27ll%20finally%20find%20his%20keys.%20%23peterfalk
+            Use python-twitter library since the user oauth is a huge undertaking to implement
         """
-        print('attempting to tweet the following message: %s' % msg)
+        print('updating twitter status with message: %s' % msg)
 
-        url_data = {'status': msg}
-        url = 'https://api.twitter.com/1.1/statuses/update.json?%s' % urllib.urlencode(url_data)
-        headers = {'Authorization': 'Bearer %s' % self.bearer_token}
-        r = requests.post(url, headers=headers)
-        if r.status_code == 200:
-            return True
-        else:
-            print(r.url)
-            print(r.status_code)
-            print(r.content)
-            return False
+        api = twitter.Api(**self.private_data['twitter']['keys'])
+        print(api.VerifyCredentials())
+        status = api.PostUpdate(msg)
+
+        print(status)
 
 
 def main():
     """
         main function, it all starts here
     """
-    prs = github_get_prs()
 
+    # check for existence of private data file
+    if not os.path.exists(PRIVATE_DATA_FILE):
+        print('private data file (%s) does not exist, please copy it here: %s' % (PRIVATE_DATA_FILE,
+                                                                                  os.path.realpath(__file__)))
+        exit(1)
+
+    t = GitTwitty()
+
+    # get a list of github prs
+    prs = t.github_get_prs()
+
+    # print separator
     print('-'*100)
 
-    t = Twitter()
-    tweets = t.get_timeline()
+    # get all tweets in the timeline
+    tweets = t.twitter_get_timeline()
 
+    # compare github prs to tweets and create a new tweet for each one that's missing missing
     for pr in prs:
         if pr in tweets:
             pass
         else:
-            t.tweet(pr)
-            # print('pr: %s not found in tweet timeline' % pr)
+            t.twitter_status_update(pr)
 
 
 if __name__ == '__main__':
